13bc8af437fbc757c62176868e3e38015a246ece
Thinh Vu <thinhvux@protonmail.com>
2024-01-15 15:56:29 +0700

    Update setup-01.sh

--- setup-01.sh
+++ setup-01.sh
@@ -6,7 +6,7 @@ apt install snapd
 snap install core
 snap install --classic certbot
 ln -s /snap/bin/certbot /usr/bin/certbot
-sudo snap set certbot trust-plugin-with-root=ok
-sudo snap install certbot-dns-route53
+snap set certbot trust-plugin-with-root=ok
+snap install certbot-dns-route53
 
 snap install docker
